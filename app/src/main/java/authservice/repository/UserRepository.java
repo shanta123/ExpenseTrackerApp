6134922dfce1a3d7c18d984ad64e9fafe5fda6a7
shanta123 <shanta.b.hiremath@gmail.com>
2025-01-16 18:32:51 +0530

    Crud repo

---
+++ app/src/main/java/authservice/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package authservice.repository;
+
+import authservice.entities.UserInfo;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserInfo, Long> {
+    public UserInfo findByUsername(String userName);
+}
