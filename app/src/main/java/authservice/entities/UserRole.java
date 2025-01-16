f516d7e819e094b71862a314c6032389385d1c27
shanta123 <shanta.b.hiremath@gmail.com>
2025-01-16 18:29:44 +0530

    Entities

---
+++ app/src/main/java/authservice/entities/UserRole.java
@@ -0,0 +1,36 @@
+package authservice.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table()
+public class UserRole {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="role_id")
+    private Long roleId;
+
+    private String name;
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
